c40060dea4553790b1aef172cdeb7bf93a3f2518
maxcore25 <mkorobov2002@mail.ru>
2022-03-09 05:54:09 +0300

    Main Component Logic Added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useState } from 'react';
 import './App.css';
+import CreateTodo from './components/CreateTodo';
+import Todos from './components/Todos';
+
+const defaultTodos = [
+  { id: 1, title: 'Learn Framer Motion', completed: false },
+  { id: 2, title: 'Create Nice Animation', completed: false },
+  { id: 3, title: 'Use Reorder Component', completed: false },
+];
 
 function App() {
-  return <div className='App'></div>;
+  const [todos, setTodos] = useState(defaultTodos);
+
+  return (
+    <div className='App'>
+      <CreateTodo onCreate={setTodos} />
+      <Todos todos={todos} />
+    </div>
+  );
 }
 
 export default App;
