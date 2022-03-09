9df61555007418d16a0675c64b5c14d1c7921df5
maxcore25 <mkorobov2002@mail.ru>
2022-03-09 06:02:02 +0300

    Create Todo Form Added

--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -1,5 +1,29 @@
 import React from 'react';
 
-export default function CreateTodo() {
-  return <div>CreateTodo</div>;
+export default function CreateTodo({ onCreate }) {
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const title = e.target.todo.value;
+
+    if (e.target.todo.value) {
+      onCreate(prevTodos => [
+        ...prevTodos,
+        {
+          title,
+          completed: false,
+          id: Date.now(),
+        },
+      ]);
+    }
+
+    e.target.reset();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} autoComplete={false}>
+      <input type='text' placeholder='New todo' name='todo' />
+      <input type='submit' value='Add Todo' />
+    </form>
+  );
 }
