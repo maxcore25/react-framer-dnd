02362918375666cfd25e18d5f9fa559bbdbf5e7d
maxcore25 <mkorobov2002@mail.ru>
2022-03-09 06:17:44 +0300

    Todo List And Item Added

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
-export default function Todos() {
-  return <div>Todos</div>;
+export default function Todos({ todos = [] }) {
+  return (
+    <ol>
+      {todos.map(todo => (
+        <TodoItem key={todo.key} todo={todo} />
+      ))}
+    </ol>
+  );
 }
+
+const TodoItem = ({ todo }) => {
+  return (
+    <li>
+      <span>{todo.title}</span>
+    </li>
+  );
+};
